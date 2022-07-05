06b52eeae580149992d2604a6d3c0e1cbd9c422e
julio cesar lopez gomez <julioneo95@hotmail.com>
2022-07-05 02:24:58 -0300

    fix: minor update in signup layout

--- src/Signup/index.jsx
+++ src/Signup/index.jsx
@@ -40,84 +40,85 @@ export function Signup({ singInUser }) {
     })
 
     return (
-        <div className="h-full flex flex-col justify-center p-12 space-y-6">
+        <div className="h-full flex justify-center items-center p-12 ">
+            <div className='flex flex-col space-y-6 w-full lg:w-1/2'>
+                <h1 className="text-3xl text-center">Crea tu cuenta </h1>
 
-            <h1 className="text-3xl">Crea tu cuenta </h1>
+                <form className="space-y-6" onSubmit={formik.handleSubmit}>
 
-            <form className="space-y-6" onSubmit={formik.handleSubmit}>
+                    <div className="space-y-2">
+                        <Input
+                            type="text"
+                            name="name"
+                            placeholder="Nombre"
+                            values={formik.values.name}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            disabled={formik.isSubmitting}
+                        />
+                        {(formik.touched.name && formik.errors.name) &&
+                            (<div className='text-red-500 text-sm'>{formik.errors.name}</div>)}
+                    </div>
 
-                <div className="space-y-2">
-                    <Input
-                        type="text"
-                        name="name"
-                        placeholder="Nombre"
-                        values={formik.values.name}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        disabled={formik.isSubmitting}
-                    />
-                    {(formik.touched.name && formik.errors.name) &&
-                        (<div className='text-red-500 text-sm'>{formik.errors.name}</div>)}
-                </div>
+                    <div className="space-y-2">
+                        <Input
+                            type="text"
+                            name="username"
+                            placeholder="Nombre de usuario"
+                            values={formik.values.username}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            disabled={formik.isSubmitting}
+                        />
+                        {(formik.touched.username && formik.errors.username) &&
+                            (<div className='text-red-500 text-sm'>{formik.errors.username}</div>)}
+                    </div>
+                    <div className="space-y-2">
+                        <Input
+                            type="text"
+                            name="email"
+                            placeholder="Email"
+                            values={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            disabled={formik.isSubmitting}
+                        />
+                        {(formik.touched.email && formik.errors.email) &&
+                            (<div className='text-red-500 text-sm'>{formik.errors.email}</div>)}
+                    </div>
+                    <div className="space-y-2">
 
-                <div className="space-y-2">
-                    <Input
-                        type="text"
-                        name="username"
-                        placeholder="Nombre de usuario"
-                        values={formik.values.username}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        disabled={formik.isSubmitting}
-                    />
-                    {(formik.touched.username && formik.errors.username) &&
-                        (<div className='text-red-500 text-sm'>{formik.errors.username}</div>)}
-                </div>
-                <div className="space-y-2">
-                    <Input
-                        type="text"
-                        name="email"
-                        placeholder="Email"
-                        values={formik.values.email}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        disabled={formik.isSubmitting}
-                    />
-                    {(formik.touched.email && formik.errors.email) &&
-                        (<div className='text-red-500 text-sm'>{formik.errors.email}</div>)}
-                </div>
-                <div className="space-y-2">
+                        <Input
+                            type="password"
+                            name="password"
+                            placeholder="Contraseña"
+                            values={formik.values.password}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            disabled={formik.isSubmitting}
+                        />
+                        {(formik.touched.password && formik.errors.password) &&
+                            (<div className='text-red-500 text-sm'>{formik.errors.password}</div>)}
 
-                    <Input
-                        type="password"
-                        name="password"
-                        placeholder="Contraseña"
-                        values={formik.values.password}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        disabled={formik.isSubmitting}
-                    />
-                    {(formik.touched.password && formik.errors.password) &&
-                        (<div className='text-red-500 text-sm'>{formik.errors.password}</div>)}
+                    </div>
 
-                </div>
-
-                <button
-                    disabled={formik.isSubmitting || !formik.isValid}
-                    type="submit"
-                    className="
+                    <button
+                        disabled={formik.isSubmitting || !formik.isValid}
+                        type="submit"
+                        className="
                 w-full 
                 bg-birdblue 
                 py-4 rounded-full 
                 disabled:opacity-50 text-lg"
-                >
-                    {formik.isSubmitting ? 'Enviendo...' : 'Registrarme'}
-                </button>
-            </form>
+                    >
+                        {formik.isSubmitting ? 'Enviendo...' : 'Registrarme'}
+                    </button>
+                </form>
 
-            <span className="text-base text-silver text-center">
-                Ya tiene cuenta?<a className="text-birdblue" href='/login'> Ingresar </a>
-            </span>
+                <div className="text-lg text-silver text-center">
+                    Ya tiene cuenta?<a className="text-birdblue" href='/login'> Ingresar </a>
+                </div>
+            </div>
         </div>
 
     )
